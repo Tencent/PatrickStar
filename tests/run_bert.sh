b4ecdefdd4c7fd866970fceb8d91f8dba7016594
jiaruifang <jiaruifang@tencent.com>
2021-07-08 13:52:08 +0800

    update run_bert.sh

--- tests/run_bert.sh
+++ tests/run_bert.sh
@@ -3,15 +3,14 @@ GPU_NUM=1
 # RES_CHECK_FLAG="--res_check"
 # Use a single GPU card to simulate multiple-GPU training.
 # FAKE_DIST="--use_fake_dist"
-# let MAX_CPU_MEMORY=12*1024*1024*1024
-# let MAX_GPU_MEMORY=4*1024*1024*1024
 
-# HYBRID_ADAM_FLAG="--use_hybrid_adam"
+
 let CHUNK_SIZE=32*1024*1024
 
 export PYTHONPATH=../:${PYTHONPATH}
 
-USE_DS_ADAM="--use_deepspeed_cpu_adam"
+# export HYBRID_ADAM_FLAG="--use_hybrid_adam"
+export USE_DS_ADAM="--use_deepspeed_cpu_adam"
 
 python ../patrickstar/launcher/runner.py --num_nodes 1 \
                              --num_gpus ${GPU_NUM} \
@@ -23,7 +22,6 @@ python ../patrickstar/launcher/runner.py --num_nodes 1 \
                              ${USE_DS_ADAM} \
                              --use_cpu_embedding \
                              --cpu_embedding_fp32 \
-                             --use_deepspeed_cpu_adam \
                              ${HYBRID_ADAM_FLAG} \
                              --default_chunk_size=${CHUNK_SIZE} \
                              2>&1 | tee log
