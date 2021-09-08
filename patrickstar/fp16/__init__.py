c5c80ff299eff696e7f7dd21a9a8fc14d6eff528
zilinzhu <zilinzhu@tencent.com>
2021-09-08 17:52:44 +0800

    Remove useless code

--- patrickstar/fp16/__init__.py
+++ patrickstar/fp16/__init__.py
@@ -11,32 +11,4 @@
 # permissions and limitations under the License.
 # See the AUTHORS file for names of contributors.
 
-from .fp16 import *
-# coding=utf-8
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-from .fp16util import (
-    bn_convert_float,
-    network_to_half,
-    prep_param_lists,
-    model_grads_to_master_grads,
-    master_params_to_model_params,
-    ToFp16,
-    to_python_float,
-    clip_grad_norm,
-    convert_module,
-    convert_network,
-    FP16Model,
-)
-from .loss_scaler import *
+from .loss_scaler import LossScaler, DynamicLossScaler
