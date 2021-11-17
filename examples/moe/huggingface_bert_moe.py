cdceae17c5de7dd428ddc32d2eb106c78f1b65f2
zilinzhu <zilinzhu@tencent.com>
2021-11-17 13:59:48 +0800

    Update MoE example to compare with native torch

--- examples/moe/huggingface_bert_moe.py
+++ examples/moe/huggingface_bert_moe.py
@@ -26,6 +26,8 @@
 # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import argparse
+from tqdm import tqdm
 
 import torch
 from torch.utils.data import DataLoader
@@ -36,6 +38,10 @@ from patrickstar.utils import get_rank
 from examples.imdb_dataset import get_dataset
 from moe_bert import build_moe_bert
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--type", dest="type", type=str, choices=["patrickstar", "torch"])
+parser.add_argument("--local_rank", dest="local_rank", type=int, default=None)
+args = parser.parse_args()
 
 torch.distributed.init_process_group(backend="nccl")
 torch.cuda.set_device(get_rank())
@@ -48,61 +54,49 @@ device = (
     else torch.device("cpu")
 )
 
+if args.type == "patrickstar":
 
-def model_func():
-    return build_moe_bert()
+    def model_func():
+        return build_moe_bert()
 
-
-config = {
-    # The same format as optimizer config of DeepSpeed
-    # https://www.deepspeed.ai/docs/config-json/#optimizer-parameters
-    "optimizer": {
-        "type": "Adam",
-        "params": {
-            "lr": 5e-5,
-            "betas": (0.9, 0.999),
-            "eps": 1e-6,
-            "weight_decay": 0,
-            "use_hybrid_adam": True,
+    config = {
+        # The same format as optimizer config of DeepSpeed
+        # https://www.deepspeed.ai/docs/config-json/#optimizer-parameters
+        "optimizer": {
+            "type": "Adam",
+            "params": {
+                "lr": 5e-5,
+                "betas": (0.9, 0.999),
+                "eps": 1e-6,
+                "weight_decay": 0,
+                "use_hybrid_adam": True,
+            },
         },
-    },
-    "default_chunk_size": 64 * 1024 * 1024,
-    "release_after_init": True,
-    "use_cpu_embedding": False,
-}
+        "default_chunk_size": 64 * 1024 * 1024,
+        "release_after_init": True,
+        "use_cpu_embedding": False,
+    }
 
-model, optim = initialize_engine(
-    model_func=model_func, local_rank=get_rank(), config=config
-)
+    model, optim = initialize_engine(
+        model_func=model_func, local_rank=args.local_rank, config=config
+    )
+else:
+    model = build_moe_bert()
+    optim = torch.optim.Adam(model.parameters(), lr=5e-5)
+    model.cuda()
+
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
-print("train loss:")
-
-for i, batch in enumerate(train_loader):
+for batch in tqdm(train_loader):
     optim.zero_grad()
     input_ids = batch["input_ids"].to(device)
     attention_mask = batch["attention_mask"].to(device)
     labels = batch["labels"].to(device)
     outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
     loss = outputs[0]
-    model.backward(loss)
+    if args.type == "patrickstar":
+        model.backward(loss)
+    else:
+        loss.backward()
     optim.step()
-    print(i, loss.item())
-    if i == 10:
-        break
-
-model.eval()
-
-print("test loss:")
-
-test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
-for i, batch in enumerate(test_loader):
-    input_ids = batch["input_ids"].to(device)
-    attention_mask = batch["attention_mask"].to(device)
-    labels = batch["labels"].to(device)
-    outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-    loss = outputs[0]
-    print(i, loss.item())
-    if i == 5:
-        break
