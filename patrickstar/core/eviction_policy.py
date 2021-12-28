d39d9dfed6df98b307138866691d06931f2c601f
zilinzhu <zilinzhu@tencent.com>
2021-12-28 10:21:29 +0800

    Remove import annotations for python3.6 support

--- patrickstar/core/eviction_policy.py
+++ patrickstar/core/eviction_policy.py
@@ -27,7 +27,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from __future__ import annotations
 from abc import ABC, abstractmethod
 from queue import PriorityQueue
 from patrickstar.core.memtracer import Metronome
